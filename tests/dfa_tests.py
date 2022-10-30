586e224929873a8e24a5b93b9eb34b1ff713c185
Martin Pilát <8pilatmartin8@gmail.com>
2022-10-30 23:35:23 +0100

    refactor of strucure, wip testing of automatons

---
+++ tests/dfa_tests.py
@@ -0,0 +1,54 @@
+import sys
+
+sys.path.append("..")
+
+from ib110hw.automaton.dfa import DFA
+from hypothesis import given
+from hypothesis.strategies import characters, booleans
+
+TMP_AUTOMATON = DFA(alphabet={"a", "b"},
+                    states={"s1", "s2", "s3"},
+                    initial_state="s1",
+                    final_states={"s2"},
+                    transitions={
+                        "s1": {
+                            "a": "s2",
+                        },
+                        "s2": {
+                            "a": "s2",
+                            "b": "s3",
+                        },
+                        "s3": {
+                            "a": "s2",
+                            "b": "s3"
+                        }
+                    })
+
+
+@given(characters(), booleans())
+def test_add_state(state: str, is_final: bool) -> None:
+    TMP_AUTOMATON.add_state(state, is_final)
+
+    assert not (is_final and state not in TMP_AUTOMATON.final_states) and \
+            state in [*TMP_AUTOMATON.states, *TMP_AUTOMATON.transitions.keys()]
+
+
+@given(characters())
+def test_remove_state(state: str) -> None:
+    TMP_AUTOMATON.remove_state(state)
+
+    assert state not in [
+        *TMP_AUTOMATON.states, 
+        *TMP_AUTOMATON.final_states, 
+        *TMP_AUTOMATON.transitions.keys(), 
+        #TODO add check of values in tranisitions
+        ]
+
+def test_add_transition() -> None:
+    pass
+
+def test_remove_transition() -> None:
+    pass
+
+def is_accepted() -> None:
+    pass
