7b48026a539d8f73b983f58a9319e727c340e244
Martin Pilát <8pilatmartin8@gmail.com>
2022-11-05 16:36:10 +0100

    added some docs to TuringMachine and delay param to simulation

--- ib110hw/turing/turing.py
+++ ib110hw/turing/turing.py
@@ -39,7 +39,7 @@ class Tape():
     def __repr__(self) -> str:
         curr_cell: Cell = self.start
 
-        # find the beggining of the tape
+        # find the leftmost cell
         while curr_cell.left:
             curr_cell = curr_cell.left
 
@@ -49,32 +49,47 @@ class Tape():
             cells.append(curr_cell)
             curr_cell = curr_cell.right
 
+        # ['a', 'b'] -> | a | b |
         str_cells = f"| {' | '.join([repr(c) for c in cells])} |\n"
-        str_marker = f"{' ' * (2 + cells.index(self.current) * 4)}^\n"
 
-        return str_cells + str_marker
+        # adds spaces to put the cursor (^) below the current cell
+        str_cursor = f"{' ' * (2 + cells.index(self.current) * 4)}^\n"
+
+        return str_cells + str_cursor
 
     def move(self, direction: Direction) -> None:
+        """Moves the tape cursor based on the provided direction.
+
+        Args:
+            direction (Direction): Specifies the move direction.
+        """
         if direction == Direction.LEFT:
             self.move_left()
         elif direction == Direction.RIGHT:
             self.move_right()
 
     def move_left(self):
+        """Moves the tape cursor to the left. (current will be current.left)
+        """
         if not self.current.left:
             self.current.left = Cell(right=self.current)
 
         self.current = self.current.left
 
     def move_right(self):
+        """Moves the tape cursor to the right. (current will be current.right)
+        """
         if not self.current.right:
             self.current.right = Cell(left=self.current)
 
         self.current = self.current.right
 
     def write(self, text: str) -> None:
-        start = self.current
+        """Writes the provided text on the tape.
 
+        Args:
+            text (str): Text to be written on the tape.
+        """
         for symbol in text:
             self.current.value = symbol
             self.move_right()
@@ -82,6 +97,8 @@ class Tape():
         self.current = self.start
 
     def clear(self) -> None:
+        """Clears the tape contents and places the cursor on the start.
+        """
         self.start = Cell()
         self.current = self.start
 
@@ -97,6 +114,7 @@ class TuringMachine():
                  rej_states: Set[str] = set(),
                  transition_function: TransitionFunction = {},
                  tape: Tape = Tape(),
+                 initial_state: str = "init",
                  start_symbol: str = ">",
                  empty_symbol: str = "") -> None:
 
@@ -111,6 +129,7 @@ class TuringMachine():
         self.acc_states = acc_states
         self.rej_states = rej_states
         self.transition_function = transition_function
+        self.initial_state = initial_state
         self.start_symbol = start_symbol
         self.empty_symbol = empty_symbol
         self.tape = tape
@@ -123,6 +142,16 @@ class TuringMachine():
                   state: str,
                   is_acc: bool = False,
                   is_rej: bool = False) -> bool:
+        """Adds state to the machine.
+
+        Args:
+            state (str): State to be added.
+            is_acc (bool, optional): True if the state is accepting. Defaults to False.
+            is_rej (bool, optional): True if the state is rejecting. Defaults to False.
+
+        Returns:
+            bool: False if the state is already present or 'is_acc' and 'is_rej' arguments are both True.
+        """
         if state in self.states or (is_acc and is_rej):
             return False
 
@@ -136,10 +165,10 @@ class TuringMachine():
 
     def remove_state(self, state: str) -> bool:
         """
-        Removes the state from the turing machine () 
+        Removes the state from the machine.
 
         Args:
-            state (str): State to be added.
+            state (str): State to be removed.
 
         Returns:
             bool: Returns True if the state was added successfully, False otherwise.
@@ -164,8 +193,20 @@ class TuringMachine():
     def simulate(self,
                  to_console: bool = True,
                  to_file: bool = False,
-                 path: str = "simulation.txt") -> bool:
-        state: str = "init"
+                 path: str = "simulation.txt",
+                 delay: float = 0.5) -> bool:
+        """ Simulates the machine on its current tape configuration.
+
+        Args:
+            to_console (bool, optional): Set to False if you only want to see the result. Defaults to True.
+            to_file (bool, optional): Set to True if you want to save every step to the file. Defaults to False.
+            path (str, optional): Path to the file with the step history. Defaults to "simulation.txt".
+            delay (float, optional): The delay (s) between each step when printing to console. Defaults to 0.5.
+
+        Returns:
+            bool: False if the machine rejects the word or exceeds the 'max_steps' value, True otherwise.
+        """
+        state: str = self.initial_state
         write: str = self.start_symbol
         steps: int = 1
         output_file = None
@@ -177,12 +218,13 @@ class TuringMachine():
             if output_file:
                 output_file.write(rule_str)
                 output_file.write(repr(self.tape))
-                output_file.write(f"\n{'=' * 40}\n\n")
+                output_file.write(f"\n{'='*40}\n\n")
 
             if to_console:
                 system("cls")
                 print(rule_str)
                 print(self.tape)
+                sleep(delay)
 
         def close():
             print_automaton_state()
@@ -212,13 +254,12 @@ class TuringMachine():
             self.tape.current.value = write
             self.tape.move(direction)
 
-            sleep(.5)
+        close()
 
         print(f"Exceeded the maximum steps allowed. ({self.max_steps})")
         print(
             "You change the default value by setting the 'max_steps' property of this automaton."
         )
-        close()
 
         return False
 
@@ -244,13 +285,18 @@ if __name__ == "__main__":
     }
 
     tape_ = Tape()
-    tape_.write(">abba")
+    tape_.write(">aba")
 
     turing = TuringMachine(
         states={"init", "space", "writeA", "writeB", "findA", "back"},
         acc_states={"finish"},
+        initial_state="init",
         input_alphabet={"a", "b", "c"},
         tape=tape_,
         transition_function=fn)
 
-    print(turing.simulate(to_file=True))
+    print(turing.simulate(delay=.05))
+
+    tape_.clear()
+    tape_.write(">abbaaabababababaa")
+    print(turing.simulate(delay=.05))
