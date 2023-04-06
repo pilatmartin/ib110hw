57b0abcbe42cebd2e121802bc76dea4b1cfbeb17
Martin Pilát <8pilatmartin8@gmail.com>
2023-04-06 02:48:53 +0200

    chore: bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ib110hw",
-    version="0.0.5",
+    version="0.0.6",
     author="Martin Pilát",
     author_email="8pilatmartin8@gmail.com",
     long_description=long_description,
